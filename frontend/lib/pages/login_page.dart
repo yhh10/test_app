2cd9f27bbfccb533f9b86f25a9bf205727293ebc
yhh10 <Hyunhoyang049@gmail.com>
2026-09-25 01:53:50 +0900

    port number modify

--- frontend/lib/pages/login_page.dart
+++ frontend/lib/pages/login_page.dart
@@ -37,7 +37,7 @@ class _LoginPageState extends State<LoginPage> {
 
     try {
       final response = await http.post(
-        Uri.parse('http://134.185.114.3/api/login'),
+        Uri.parse('http://134.185.114.3:8000/api/login'),
         headers: {'Content-Type': 'application/json'},
         body: jsonEncode({
           'id' : _idController.text,
