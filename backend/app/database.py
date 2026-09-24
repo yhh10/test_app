4ae0f20436b6cf91064c108e66ec056d81b9963f
yhh10 <Hyunhoyang049@gmail.com>
2026-09-25 01:09:10 +0900

    database.py modify

--- backend/app/database.py
+++ backend/app/database.py
@@ -1,10 +1,9 @@
 from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from config import Config
 from sqlalchemy.orm import DeclarativeBase
 
-engine = Config.SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread":False}
+engine = create_engine(Config.SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread":False})
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 def get_db():
